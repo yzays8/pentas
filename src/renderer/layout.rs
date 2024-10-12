2d895db8da411d02fbbb4b6194bef635b1146ae1
yzays8 <yzays8@gmail.com>
2024-10-13 06:05:20 +0900

    Fix layout

--- src/renderer/layout.rs
+++ src/renderer/layout.rs
@@ -39,8 +39,7 @@ impl BoxTree {
 
         Ok(Self {
             root: Rc::new(RefCell::new(
-                BoxNode::build(root.unwrap(), FormattingContext::Block)
-                    .context("Failed to build box tree")?,
+                BoxNode::build(root.unwrap()).context("Failed to build box tree")?,
             )),
         })
     }
@@ -76,11 +75,11 @@ impl BoxTree {
                     // 4. Any space immediately following another space (even across two separate inline elements) is ignored.
                     .replace_all(&text, " ");
 
-                let text = match node.borrow().ctx {
+                let text = match tnode.borrow().get_display_type() {
                     // 5. All spaces at the beginning and end of the block box are removed.
-                    FormattingContext::Block => text.trim(),
-                    // 5': Sequences of spaces at the beginning and end of an element are removed.
-                    FormattingContext::Inline => {
+                    DisplayOutside::Block => text.trim(),
+                    // 5'. Sequences of spaces at the beginning and end of an element are removed.
+                    DisplayOutside::Inline => {
                         if is_first_child {
                             text.trim_start()
                         } else if is_last_child {
@@ -186,21 +185,6 @@ impl fmt::Display for BoxTree {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum FormattingContext {
-    Block,
-    Inline,
-}
-
-impl fmt::Display for FormattingContext {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            FormattingContext::Block => write!(f, "Block"),
-            FormattingContext::Inline => write!(f, "Inline"),
-        }
-    }
-}
-
 /// Generated box type from the `display` property.
 #[derive(Debug)]
 pub enum BoxKind {
@@ -220,77 +204,76 @@ pub enum BoxKind {
 #[derive(Debug)]
 pub struct BoxNode {
     pub box_kind: BoxKind,
-    pub ctx: FormattingContext,
     pub child_nodes: Vec<Rc<RefCell<Self>>>,
 }
 
 impl BoxNode {
-    pub fn build(node: Rc<RefCell<RenderNode>>, current_ctx: FormattingContext) -> Option<Self> {
+    pub fn build(node: Rc<RefCell<RenderNode>>) -> Option<Self> {
         match node.borrow().node.borrow().node_type {
             NodeType::Comment(_) | NodeType::DocumentType(_) => return None,
             NodeType::Text(_) => {
                 return Some(Self {
                     box_kind: BoxKind::Text(Rc::clone(&node)),
-                    ctx: current_ctx,
                     child_nodes: vec![],
                 })
             }
             _ => {}
         }
 
-        // Formatting context for the children
-        let next_ctx = if node
-            .borrow()
-            .child_nodes
-            .iter()
-            .any(|child| child.borrow().get_display_type() == DisplayOutside::Block)
-        {
-            FormattingContext::Block
-        } else {
-            FormattingContext::Inline
+        let mut ret = Self {
+            box_kind: match node.borrow().get_display_type() {
+                DisplayOutside::Block => BoxKind::Block(Rc::clone(&node)),
+                DisplayOutside::Inline => BoxKind::Inline(Rc::clone(&node)),
+            },
+            child_nodes: Vec::new(),
         };
-
-        let mut children: Vec<Rc<RefCell<BoxNode>>> = Vec::new();
         let mut i = 0;
         while i < node.borrow().child_nodes.len() {
             match node.borrow().child_nodes[i].borrow().get_display_type() {
                 DisplayOutside::Block => {
-                    let child = Self::build(Rc::clone(&node.borrow().child_nodes[i]), next_ctx);
+                    if node.borrow().get_display_type() == DisplayOutside::Inline {
+                        // todo: It is tricky to handle block-level boxes within an inline box.
+                        // https://www.w3.org/TR/CSS22/visuren.html#anonymous-block-level
+                        // https://github.com/w3c/csswg-drafts/issues/1477
+                        unimplemented!(
+                            "Block-level boxes within an inline box are not yet supported: {}",
+                            node.borrow().node.borrow().node_type
+                        );
+                    }
+
+                    let child = Self::build(Rc::clone(&node.borrow().child_nodes[i]));
                     if let Some(child) = child {
-                        children.push(Rc::new(RefCell::new(child)));
+                        ret.child_nodes.push(Rc::new(RefCell::new(child)));
                     }
                 }
                 DisplayOutside::Inline => {
-                    if (FormattingContext::Block == current_ctx)
+                    if (node.borrow().get_display_type() == DisplayOutside::Block)
                         && (node.borrow().child_nodes.len() > 1)
                     {
                         // Inline-level contents in block formatting context are wrapped in anonymous boxes.
                         let mut anon_box = BoxNode {
                             box_kind: BoxKind::Anonymous,
-                            ctx: FormattingContext::Block,
                             child_nodes: vec![],
                         };
 
                         // If there are successive inline-level contents, they are wrapped in the same anonymous box.
+                        // https://www.w3.org/TR/css-inline-3/#root-inline-box
                         while i < node.borrow().child_nodes.len()
                             && node.borrow().child_nodes[i].borrow().get_display_type()
                                 == DisplayOutside::Inline
                         {
-                            let child = Self::build(
-                                Rc::clone(&node.borrow().child_nodes[i]),
-                                FormattingContext::Inline,
-                            );
+                            let child = Self::build(Rc::clone(&node.borrow().child_nodes[i]));
                             if let Some(child) = child {
                                 anon_box.child_nodes.push(Rc::new(RefCell::new(child)));
                             }
                             i += 1;
                         }
                         i -= 1;
-                        children.push(Rc::new(RefCell::new(anon_box)));
+                        ret.child_nodes.push(Rc::new(RefCell::new(anon_box)));
                     } else {
-                        let child = Self::build(Rc::clone(&node.borrow().child_nodes[i]), next_ctx);
+                        let child = Self::build(Rc::clone(&node.borrow().child_nodes[i]));
                         if let Some(child) = child {
-                            children.push(Rc::new(RefCell::new(child)));
+                            ret.child_nodes.push(Rc::new(RefCell::new(child)));
                         }
                     }
                 }
@@ -299,24 +282,17 @@ impl BoxNode {
             i += 1;
         }
 
-        Some(Self {
-            box_kind: match node.borrow().get_display_type() {
-                DisplayOutside::Block => BoxKind::Block(Rc::clone(&node)),
-                DisplayOutside::Inline => BoxKind::Inline(Rc::clone(&node)),
-            },
-            ctx: current_ctx,
-            child_nodes: children,
-        })
+        Some(ret)
     }
 }
 
 impl fmt::Display for BoxNode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self.box_kind {
-            BoxKind::Block(node) => write!(f, "Ctx: {}, Box: Block, {}", self.ctx, node.borrow()),
-            BoxKind::Inline(node) => write!(f, "Ctx: {}, Box: Inline, {}", self.ctx, node.borrow()),
-            BoxKind::Text(node) => write!(f, "Ctx: {}, {}", self.ctx, node.borrow()),
-            BoxKind::Anonymous => write!(f, "Ctx: {}, Box: Anonymous", self.ctx),
+            BoxKind::Block(node) => write!(f, "Box: Block, {}", node.borrow()),
+            BoxKind::Inline(node) => write!(f, "Box: Inline, {}", node.borrow()),
+            BoxKind::Text(node) => write!(f, "{}", node.borrow()),
+            BoxKind::Anonymous => write!(f, "Box: Anonymous"),
         }
     }
 }
